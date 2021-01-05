78ed1f236e8e3e771b5be48ef26ae49d6b78f089
Stefan Pruneanu <stefanpruneanu@gmail.com>
2021-01-05 17:30:31 +0200

    sort posts based on month

--- app/src/PostsOverview.js
+++ app/src/PostsOverview.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import {gql, useLazyQuery, useQuery} from '@apollo/client';
+import {gql, useQuery} from '@apollo/client';
 
 function PostsOverview(props) {
   const GET_ALL_POSTS = gql`
       query Query($nrPosts: Int!) {
         allPosts(count: $nrPosts) {
-          id
-          title
           createdAt 
       }
     }
@@ -17,8 +15,32 @@ function PostsOverview(props) {
     variables: { nrPosts: props.postsToFetch },
   });
 
+  const postsPerMonth = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0}
+
   function showPosts() {
-    console.log(data.allPosts)
+    // go through each post from the query and count it
+    for (const post of data.allPosts) {
+      const date = new Date(parseInt(post.createdAt))
+      const month = date.getMonth()
+
+      postsPerMonth[month]+=1;
+    }
+
+    // make graph and return it
+    // return <graph>
+
   }
 
   if (loading) return 'Loading...';
